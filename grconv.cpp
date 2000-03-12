171a32bf69cf81a4ec95da6539a344c57027bb64
Diomidis Spinellis <dds@aueb.gr>
2000-03-12 13:13:16 +0000

    Major structural changes, before release.

--- grconv.cpp
+++ grconv.cpp
@@ -1,3 +1,19 @@
+/* 
+ * (C) Copyright 2000 Diomidis Spinellis.
+ * 
+ * Permission to use, copy, and distribute this software and its
+ * documentation for any purpose and without fee is hereby granted,
+ * provided that the above copyright notice appear in all copies and that
+ * both that copyright notice and this permission notice appear in
+ * supporting documentation.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+ * MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * $Id: grconv.cpp,v 1.3 2000/03/12 13:13:16 dds Exp $
+ */
+
 #include <stdlib.h>
 #include <string.h>
 
@@ -43,20 +59,21 @@ void
 usage()
 {
 	error("usage:\n");
-	cerr << "grconv [-S enc] [-s cs] [-t cs] [-T enc] [-h] [-d c]\n"
-			"grconv [-S enc] [-s cs] -x xl [-d c]\n"
-			"grconv -r [-t cs] [-T enc] [-h] [-d c]\n"
-			"grconv -v|-L\n"
-			"\t-S enc\tSpecify source encoding (default to 8bit or UCS-2)\n"
-			"\t-s cs\tSpecify source character set (default to ISO-8859-7)\n"
-			"\t-T enc\tSpecify target encoding (default to 8bit or UCS-2)\n"
-			"\t-t cs\tSpecify target character set (default to ISO-8859-7)\n"
-			"\t-x xl\tSpecify one of transcribe or transliterate (ISO 843:1997)\n"
-			"\t-r\tPerform reverse transliteration\n"
-			"\t-h\tCreate header for the output encoding\n"
-			"\t-d char\tSpecify character for unknown mappings (default to space)\n"
-			"\t-v\tDisplay program version and copyright\n"
-			"\t-L\tList supported encodings\n";
+	cerr <<
+	"grconv [-S enc] [-s cs] [-t cs] [-T enc] [-h] [-d c] [file...]\n"
+	"grconv [-S enc] [-s cs] -x xl [-d c] [file...]\n"
+	"grconv -r [-t cs] [-T enc] [-h] [-d c] [file...]\n"
+	"grconv -v|-L\n"
+	"\t-S enc\tSpecify source encoding (default to 8bit or UCS-2)\n"
+	"\t-s cs\tSpecify source character set (default to ISO-8859-7)\n"
+	"\t-T enc\tSpecify target encoding (default to 8bit or UCS-2)\n"
+	"\t-t cs\tSpecify target character set (default to ISO-8859-7)\n"
+	"\t-x xl\tSpecify one of transcribe or transliterate (ISO 843:1997)\n"
+	"\t-r\tPerform reverse transliteration\n"
+	"\t-h\tCreate header for the output encoding\n"
+	"\t-d char\tSpecify character for unknown mappings (default to space)\n"
+	"\t-v\tDisplay program version and copyright\n"
+	"\t-L\tList supported character sets and encodings\n";
 	exit(1);
 }
 
@@ -64,19 +81,46 @@ static void
 encodings()
 {
 	cout << "Valid input/output encodings are:\n"
-		"\tFor Unicode data: UCS-2 UTF-8 UTF-7 Java RTF HTML\n"
+		"\tFor Unicode data: UCS-2 UTF-8 UTF-7 Java HTML\n"
 		"\tFor 8-bit data: 8bit Base64 Quoted RTF HTML HTML-Lat\n"
 	"Valid character sets are:\n";
-	for (struct s_charset *cp = charsets; cp->name; cp++)
-		cout << cp->name << " ";
-	cout << "(many character set names are aliases)\n";
+	int margin=0;
+	for (struct s_charset *cp = charsets; cp->name; cp++) {
+		cout << cp->name;
+		if (cp[1].name)
+			cout << ", ";
+		if ((margin += strlen(cp->name) + 2) > 65) {
+			margin = 0;
+			cout << "\n";
+		}
+	}
+	cout << "\nMany of the character set names are aliases.\n";
 	cout << "Transcription/transliteration are performed only to plain ASCII.\n";
+}
+
+static void
+version()
+{
+	cout << 
+	"Universal Greek character code converter.  Version " VERNAME "\n"
+	"(C) Copyright 2000 Diomidis D. Spinelllis.  All rights reserved.\n\n"
+
+	"Permission to use, copy, and distribute this software and its\n"
+	"documentation for any purpose and without fee is hereby granted,\n"
+	"provided that the above copyright notice appear in all copies and that\n"
+	"both that copyright notice and this permission notice appear in\n"
+	"supporting documentation.\n\n"
+
+	"THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED\n"
+	"WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF\n"
+	"MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.\n";
 }
 
 int
 main(int argc, char *argv[])
 {
-	filter *f = new stdinput;		// Current pipeline input
+	stdinput in;
+	filter *f = &in;			// Current pipeline input
 	char *sourcecs = NULL;
 	char *targetcs = NULL;
 	char defchar = ' ';
@@ -95,19 +139,7 @@ main(int argc, char *argv[])
 			encodings();
 			exit(0);
 		case 'v':
-			cout << 
-	"Universal Greek character code converter.  $Revision: 1.2 $\n"
-	"(C) Copyright 2000 Diomidis D. Spinelllis\n\n"
-
-	"Permission to use, copy, and distribute this software and its\n"
-	"documentation for any purpose and without fee is hereby granted,\n"
-	"provided that the above copyright notice appear in all copies and that\n"
-	"both that copyright notice and this permission notice appear in\n"
-	"supporting documentation.\n\n"
-
-	"THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED\n"
-	"WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF\n"
-	"MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.\n";
+			version();
 			exit(0);
 		case 'd':
 			if (!optarg || optarg[1]) {
@@ -204,8 +236,7 @@ main(int argc, char *argv[])
 		}
 
 
-	if (argv[optind] != NULL)
-		usage();
+	in.setargs(argv + optind);
 
 	// Test for incompatible flags
 	if ((xflag && (targetcs || oenc)) ||
